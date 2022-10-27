1dffd3066bb13f9deebc08f916b20c46a4d323c0
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-10-27 19:10:24 -0300

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-  })
-}
+// if (!firebase.apps.length) {
+//   firebase.initializeApp({
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+//   })
+// }
 
-export default firebase
+// export default firebase
 
 // import { initializeApp } from "firebase/app";
 
@@ -24,3 +24,17 @@ export default firebase
 // };
 
 // const app = initializeApp(firebaseConfig);
+//
+
+import firebase from "firebase/app"
+import 'firebase/auth'
+
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    })
+}
+
+export default firebase
