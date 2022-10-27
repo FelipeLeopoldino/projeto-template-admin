b9d8f16f494756a8915e61fe0e2be1c74c5fc336
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-10-27 19:42:11 -0300

    .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 
-    apiKey: 'AIzaSyAPd3np2VK6Kk9S-hm7y8p7kBpMxCYts1Y',
-    authDomain: 'admin-template-cc164.firebaseapp.com',
-    projectId: 'admin-template-cc164'
+    // apiKey: 'AIzaSyAPd3np2VK6Kk9S-hm7y8p7kBpMxCYts1Y',
+    // authDomain: 'admin-template-cc164.firebaseapp.com',
+    // projectId: 'admin-template-cc164'
   })
 }
 
