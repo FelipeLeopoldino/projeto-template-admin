6b94c33252ccdfb819f218b6787bd540a86127d8
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-10-27 19:13:32 -0300

    apikey

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,40 +1,16 @@
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
-
-// if (!firebase.apps.length) {
-//   firebase.initializeApp({
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-//   })
-// }
-
-// export default firebase
-
-// import { initializeApp } from "firebase/app";
-
-
-// const firebaseConfig = {
-//   apiKey:  process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: "admin-template-cc164.appspot.com",
-//   messagingSenderId: "161926539977",
-//   appId: "1:161926539977:web:0c5269b7a533d34e073b18"
-// };
-
-// const app = initializeApp(firebaseConfig);
-//
-
-import firebase from "firebase/app"
+import firebase from 'firebase/app'
 import 'firebase/auth'
 
 if (!firebase.apps.length) {
-    firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    })
+  firebase.initializeApp({
+    // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
+    apiKey: 'AIzaSyAPd3np2VK6Kk9S-hm7y8p7kBpMxCYts1Y',
+    authDomain: 'admin-template-cc164.firebaseapp.com',
+    projectId: 'admin-template-cc164'
+  })
 }
 
 export default firebase
