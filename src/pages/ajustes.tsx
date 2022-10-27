849e6aa2941db04cc4a25771c62ec7d4eb8086e9
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-10-27 19:28:59 -0300

    botão tema mobile

--- src/pages/ajustes.tsx
+++ src/pages/ajustes.tsx
@@ -1,10 +1,20 @@
-import Layout from "../components/template/Layout";
-
+import BotaoAlternarTema from '../components/template/BotaoAlternarTema'
+import Layout from '../components/template/Layout'
+import useAppData from '../data/hook/UseAppData'
 
 export default function Ajustes() {
+  const { tema, alternarTema } = useAppData()
   return (
-    <Layout titulo='Ajustes e Configurações' subtitulo="Personalize o sistema por aqui">
-      <h3>Conteúdo</h3>
+    <Layout
+      titulo="Ajustes e Configurações"
+      subtitulo="Personalize o sistema por aqui"
+    >
+      <div className="flex justify-center">
+        <h3>Conteúdo</h3>
+        <span className='ml-5'>
+          <BotaoAlternarTema tema={tema} alternaTema={alternarTema} />
+        </span>
+      </div>
     </Layout>
   )
 }
