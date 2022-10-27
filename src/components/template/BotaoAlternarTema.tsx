7b9cd27fa97f44a084318759a2d537ae2eb14d69
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-10-27 19:30:06 -0300

    botao tema mobile

--- src/components/template/BotaoAlternarTema.tsx
+++ src/components/template/BotaoAlternarTema.tsx
@@ -10,7 +10,7 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps) {
     <div
       onClick={props.alternaTema}
       className={`
-     sm:flex items-center cursor-pointer
+    sm:flex items-center cursor-pointer
     bg-gradient-to-r from-yellow-300 to-yellow-600
     w-14 lg:w-24 h-8 p-1 rounded-full
     `}
@@ -37,7 +37,7 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps) {
     <div
       onClick={props.alternaTema}
       className={`
-      hidden sm:flex items-center justify-end cursor-pointer
+       sm:flex items-center justify-end cursor-pointer
       bg-gradient-to-r from-gray-500 to-gray-900
       w-14 lg:w-24 h-8 p-1 rounded-full
         `}
